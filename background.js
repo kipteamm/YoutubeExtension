c64a857b7e3f0f0797a3eee88b3d2380ba407ed8
DevVali <90271108+DevVali@users.noreply.github.com>
2024-07-20 21:46:01 +0200

    feat(app): instagram support

--- background.js
+++ background.js
@@ -1,25 +1,56 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ youtubeVisitAllowed: false });
+    chrome.storage.local.set({
+        youtubeVisitAllowed: false,
+        instaVisitAllowed: false,
+    });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab && tab.url) {
-        chrome.storage.local.get('youtubeVisitAllowed', (result) => {
-            if (!tab.url.includes('youtube.com') && !tab.url.includes('youtu.be')) return;
+    if (changeInfo.status === "complete" && tab && tab.url) {
+        // YouTube
+        chrome.storage.local.get("youtubeVisitAllowed", (result) => {
+            if (
+                !tab.url.includes("youtube.com") &&
+                !tab.url.includes("youtu.be")
+            )
+                return;
 
             const youtubeVisitAllowed = result.youtubeVisitAllowed || false;
 
             if (!youtubeVisitAllowed) {
-                chrome.tabs.update(tabId, { url: chrome.runtime.getURL('warning.html') });
+                chrome.tabs.update(tabId, {
+                    url: chrome.runtime.getURL("warning_youtube.html"),
+                });
+            }
+        });
+
+        // Instagram
+        chrome.storage.local.get("instaVisitAllowed", (result) => {
+            if (!tab.url.includes("instagram.com")) return;
+
+            const instaVisitAllowed = result.instaVisitAllowed || false;
+
+            if (!instaVisitAllowed) {
+                chrome.tabs.update(tabId, {
+                    url: chrome.runtime.getURL("warning_insta.html"),
+                });
             }
         });
     }
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    chrome.tabs.query({ url: '*://*.youtube.com/*' }, (tabs) => {
+    // YouTube
+    chrome.tabs.query({ url: "*://*.youtube.com/*" }, (tabs) => {
         if (tabs.length === 0) {
             chrome.storage.local.set({ youtubeVisitAllowed: false });
+        }
+    });
+
+    // Instagram
+    chrome.tabs.query({ url: "*://*.instagram.com/*" }, (tabs) => {
+        if (tabs.length === 0) {
+            chrome.storage.local.set({ instaVisitAllowed: false });
         }
     });
 });
--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function openYoutube() {
     chrome.storage.local.set({ youtubeVisitAllowed: true }, () => {
-        return window.location.href = 'https://www.youtube.com';
+        return (window.location.href = "https://www.youtube.com");
     });
 }
 
-document.getElementById('openYoutube').addEventListener('click', openYoutube);
+function openInsta() {
+    chrome.storage.local.set({ instaVisitAllowed: true }, () => {
+        return (window.location.href = "https://www.instagram.com");
+    });
+}
+
+if (document?.getElementById("openYoutube")) {
+    document
+        .getElementById("openYoutube")
+        .addEventListener("click", openYoutube);
+}
+
+if (document?.getElementById("openInsta")) {
+    document.getElementById("openInsta").addEventListener("click", openInsta);
+}
 
 function closeTab() {
     return window.close();
 }
 
-document.getElementById('closeTab').addEventListener('click', closeTab);
+document.getElementById("closeTab").addEventListener("click", closeTab);
